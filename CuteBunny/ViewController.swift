f95c739207781bed4e8215db47babc09f92c52cd
JinUk Baek <chiggang@gmail.com>
2016-09-16 21:07:58 +0900

    Initial Commit

---
+++ CuteBunny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CuteBunny
+//
+//  Created by JinUk Baek on 2016. 9. 16..
+//  Copyright © 2016년 chiggang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
