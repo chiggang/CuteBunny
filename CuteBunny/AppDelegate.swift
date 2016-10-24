46316f6c9d04020d16c1e2b082b29faec065600f
YouSung Jang <tkword@gmail.com>
2016-10-24 10:49:35 -0400

    commit test

--- CuteBunny/AppDelegate.swift
+++ CuteBunny/AppDelegate.swift
@@ -6,13 +6,14 @@
 //  Copyright © 2016년 chiggang. All rights reserved.
 //
 
+// Test by Jang from the US :)
+
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
-
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
@@ -40,7 +41,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func applicationWillTerminate(_ application: UIApplication) {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 	}
-
-
 }
 
